23acce25717a7726aeef9dd3f564adaeed980539
JimothyMarmalade <logan.edmund@gmail.com>
2021-01-29 18:03:16 -0600

    Player Functionality
    Added the Player, with the ability to move and detect treasures buried underground

---
+++ CachOutUnityDev/Assets/Scripts/MetalDetector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetalDetector : MonoBehaviour
+{
+    public DetectorRange CloseProxy;
+    public DetectorRange MedProxy;
+    public DetectorRange FarProxy;
+
+    public int DetectionFrequency = 0;
+
+    void Update()
+    {
+        if (CloseProxy.GetTreasureStatus())
+            DetectionFrequency = 3;
+        else if (MedProxy.GetTreasureStatus())
+            DetectionFrequency = 2;
+        else if (FarProxy.GetTreasureStatus())
+            DetectionFrequency = 1;
+        else
+            DetectionFrequency = 0;
+
+        Debug.Log("DetectionFrequency is " + DetectionFrequency);
+    }
+}
